1fb6bf95d36a971ccbe2c45bb8188e901e7c913e
Vladik <optimus014@gmail.com>
2019-04-08 23:19:48 +0300

    Updates

---
+++ precache-manifest.b6118387985a334ed29218e0e4ed1538.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5423a8f951f1c7369384",
+    "url": "/swapi-react/static/js/runtime~main.615c9c00.js"
+  },
+  {
+    "revision": "81f8a30ab27215301ca0",
+    "url": "/swapi-react/static/js/main.0cc1bb50.chunk.js"
+  },
+  {
+    "revision": "1bbfdc48a36411083b32",
+    "url": "/swapi-react/static/js/2.f6d295a9.chunk.js"
+  },
+  {
+    "revision": "81f8a30ab27215301ca0",
+    "url": "/swapi-react/static/css/main.b8d03702.chunk.css"
+  },
+  {
+    "revision": "5a59b7d7c278eb6a4398cd1405e08c16",
+    "url": "/swapi-react/index.html"
+  }
+];
